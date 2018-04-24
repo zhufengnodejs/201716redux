f4313cd35ffe9a7c496de3a0bd3755a619819d8f
zhufengnodejs <zhufengnodejs@126.com>
2018-04-24 12:22:52 +0800

    commit

--- src/counter/components/Counter.js
+++ src/counter/components/Counter.js
@@ -1,11 +1,29 @@
 import React,{Component} from 'react';
+import store from '../store';
+import * as types from '../store/action-types';
 export default class Counter extends Component{
+      //state = {number:0} 相当于在构造函数this.state = {number:0}
+     constructor(){
+         super();
+         this.state = {number:store.getState().number};
+     }
+    componentDidMount(){
+         this.unsubscribe = store.subscribe(()=>{
+             this.setState({number:store.getState().number});
+         })
+    }
+    componentWillUnmount(){
+        this.unsubscribe();
+    }
     render(){
         return (
             <div>
-                <p></p>
-                <button>+</button>
-                <button>-</button>
+                <p>{this.state.number}</p>
+                <button
+                    onClick={()=>store.dispatch({type:types.INCREMENT})}>+</button>
+                <button
+                    onClick={()=>store.dispatch({type:types.DECREMENT})}
+                >-</button>
             </div>
         )
     }
