9ebb50fc54146a3c2db63116239c17eacf800ca7
zhufengnodejs <zhufengnodejs@126.com>
2018-04-24 15:48:40 +0800

    commit

--- src/counter/components/Counter1.js
+++ src/counter/components/Counter1.js
@@ -2,37 +2,47 @@ import React,{Component} from 'react';
 import {bindActionCreators} from 'redux';
 import store from '../store';
 import actions from '../store/actions/counter1';
-let newActions = bindActionCreators(actions,store.dispatch);
-
-export default class Counter extends Component{
-      //state = {number:0} 相当于在构造函数this.state = {number:0}
-     constructor(){
-         super();
-         this.state = {number:store.getState().counter1.number};
-     }
-    componentDidMount(){
-         this.unsubscribe = store.subscribe(()=>{
-             this.setState({number:store.getState().counter1.number});
-         })
-    }
-    componentWillUnmount(){
-        this.unsubscribe();
-    }
+//用来实现连接 连接仓库和我们的组件
+import {connect} from 'react-redux';
+//let newActions = bindActionCreators(actions,store.dispatch);
+//纯UI组件 里面没有状态
+class Counter extends Component{
     render(){
         return (
             <div>
-                <p>{this.state.number}</p>
+                <p>{this.props.number}</p>
                 <button
-                    onClick={newActions.increment}>+</button>
+                    onClick={this.props.increment}>+</button>
                 <button
-                    onClick={newActions.decrement}
+                    onClick={this.props.decrement}
                 >-</button>
             </div>
         )
     }
 }
-
-
+//把一个状态(合并后的state)对象映射为一个当前组件的属性对象
+//本质是输入，把仓库中的状态输入到组件中，让组件显示仓库中的状态数据
+/**
+ * {
+ *  counter1:{number:3},
+ *  counter2:{number:5},
+ * }
+ */
+const mapStateToProps = (state)=> state.counter1;
+//把仓库的dispatch方法映射为一个属性对象
+//输出 把用户在组件中的操作输出出去
+//const mapDispatchToProps = dispatch=> bindActionCreators(actions,dispatch)
+/**
+ * {
+ *    increment(){dispatch({type:'INCREMENT'})}
+ *    decrement(){dispatch({type:'DECREMENT'}}
+ * }
+ */
+export default connect(
+    mapStateToProps,
+    //dispatch=> bindActionCreators(actions,dispatch)
+    actions
+)(Counter);
 
 
 
--- src/counter/components/Counter2.js
+++ src/counter/components/Counter2.js
@@ -1,8 +1,8 @@
 import React,{Component} from 'react';
 import {bindActionCreators} from 'redux';
-import store from '../store';
 import actions from '../store/actions/counter2';
-let newActions = bindActionCreators(actions,store.dispatch);
+import {connect} from 'react-redux';
+//let newActions = bindActionCreators(actions,store.dispatch);
 
 /**
  * React组件和仓库之间的连接进行关联
@@ -10,30 +10,21 @@ let newActions = bindActionCreators(actions,store.dispatch);
  * 2. 把在组件中发生的动作发射出去
  * 3. 组件还要订阅仓库中的状态变化事件，当状态变化的时候重新渲染组件
  */
-export default class Counter extends Component{
-      //state = {number:0} 相当于在构造函数this.state = {number:0}
-     constructor(){
-         super();
-         this.state = {number:store.getState().counter2.number};
-     }
-    componentDidMount(){
-         this.unsubscribe = store.subscribe(()=>{
-             this.setState({number:store.getState().counter2.number});
-         })
-    }
-    componentWillUnmount(){
-        this.unsubscribe();
-    }
+class Counter extends Component{
     render(){
         return (
             <div>
-                <p>{this.state.number}</p>
+                <p>{this.props.number}</p>
                 <button
-                    onClick={newActions.increment}>+</button>
+                    onClick={this.props.increment}>+</button>
                 <button
-                    onClick={newActions.decrement}
+                    onClick={this.props.decrement}
                 >-</button>
             </div>
         )
     }
 }
+export default connect(
+    state=>state.counter2,
+    actions
+)(Counter);
