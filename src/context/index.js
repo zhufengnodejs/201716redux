939a1a22212a9d3abfebf3ccaa7d5bb6bdda9b31
zhufengnodejs <zhufengnodejs@126.com>
2018-04-25 16:13:05 +0800

    commit

--- src/context/index.js
+++ src/context/index.js
@@ -1,16 +1,31 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
+import propTypes from 'prop-types';
 class Title extends Component{
+    //表示要从上下文对象中接收color属性
+    static contextTypes = {
+        color:propTypes.string
+    }
     render(){
+        console.log(this.context);
         return (
-            <div>标题</div>
+            <div style={{color:this.context.color}}>标题</div>
         )
     }
 }
 class Content extends Component{
+    //表示要从上下文对象中接收color属性
+    static contextTypes = {
+        color:propTypes.string,
+        changeColor:propTypes.func
+    }
     render(){
         return (
-            <div>内容</div>
+            <div style={{color:this.context.color}}>
+                内容
+                <button onClick={()=>this.context.changeColor('green')}>变绿</button>
+                <button onClick={()=>this.context.changeColor('yellow')}>变黄</button>
+            </div>
         )
     }
 }
@@ -29,6 +44,19 @@ function Body(){
     )
 }
 class Page extends Component{
+  state = {color:'red'}
+  //规定了下层上下文对象的名称和类型
+  static childContextTypes = {
+      color:propTypes.string,
+      name:propTypes.string,
+      changeColor:propTypes.func
+  }
+  changeColor = (color)=>{
+      this.setState({color});
+  }
+  getChildContext(){
+    return {name:'zfpx',color:this.state.color,changeColor:this.changeColor};
+  }
   render(){
       return (
           <div>
---
+++ src/context/test.js
@@ -0,0 +1,9 @@
+class Person{
+    name='zfpx'
+    getName(){
+
+    }
+}
+let p = new Person();
+console.log(p.name);
+console.log(p.__proto__.getName);
