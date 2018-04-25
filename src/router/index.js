a8b951348605adad2151d13a1a29868e3d0aa961
zhufengnodejs <zhufengnodejs@126.com>
2018-04-25 17:36:01 +0800

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,21 @@
 //如何通过派发动作的方式跳转路径
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter as Router,Route,Link} from 'react-router-dom';
-
+import {Route,Link} from 'react-router-dom';
+import {ConnectedRouter, routerReducer, routerMiddleware, push}  from 'react-router-redux';
+import {Provider} from 'react-redux';
+import {createStore,combineReducers,applyMiddleware} from 'redux';
+import createHistory from 'history/createHashHistory';
+let history = createHistory();
+let counter = function(state=0,action){
+   return state;
+}
+//合并reducers
+let reducers = combineReducers({
+    counter,
+    router:routerReducer
+});
+let store = createStore(reducers);
 function Home(){
     return <div>Home</div>
 }
@@ -13,12 +26,14 @@ function Profile(){
     return <div>Profile</div>
 }
 ReactDOM.render(
-        <Router>
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
             <div>
                 <Route exact path="/" component={Home}/>
                 <Route path="/user" component={User}/>
                 <Route path="/profile" component={Profile}/>
             </div>
-        </Router>
+        </ConnectedRouter>
+    </Provider>
     ,document.querySelector('#root')
 );
