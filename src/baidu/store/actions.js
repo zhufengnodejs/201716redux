723fa05666c4e2d4ad7dfd4b5ba3b7a6c3016890
zhufengnodejs <zhufengnodejs@126.com>
2018-04-25 12:26:13 +0800

    commit

--- src/baidu/store/actions.js
+++ src/baidu/store/actions.js
@@ -1,20 +1,25 @@
 import $ from 'jquery';
+import * as types from './action-types';
 export default {
     //先得到关键字，然后请求百度接口得到返回值,然后再改变状态
     //1,没有返回可用的action对象
     //2.没有dispatch方法，无法派发
+    //query方法以前返回的是一个普通 对象，现在返回的是一个函数，而且会向函数里传入dispatch和getStage方法
     query(wd){
-        $.ajax({
-            url:'http://www.baidu.com/su',
-            method:'GET',
-            data:{wd},
-            jsonp:'cb',//指定的后台用来接口方法名的参数名
-            dataType:'jsonp',//指定返回值的类型
-            success:(data)=>{
-              let words = data.s;
-
-            }
-        });
+        return function(dispatch,getState){
+            console.log(arguments);
+            $.ajax({
+                url:'http://www.baidu.com/su',
+                method:'GET',
+                data:{wd},
+                jsonp:'cb',//指定的后台用来接口方法名的参数名
+                dataType:'jsonp',//指定返回值的类型
+                success:(data)=>{
+                    let words = data.s;
+                    dispatch({type:types.WORDS,words});
+                }
+            });
+        }
     }
 }
 //Error: Actions must be plain objects. Use custom middleware for async actions.
--- src/baidu/store/index.js
+++ src/baidu/store/index.js
@@ -1,4 +1,6 @@
-import {createStore} from 'redux';
+import {createStore,applyMiddleware} from 'redux';
 import reducer from './reducer';
-let store = createStore(reducer);
+//这个中间件的作用就是可以让store派发函数
+import thunk from 'redux-thunk';
+let store = createStore(reducer,applyMiddleware(thunk));
 export default store;
